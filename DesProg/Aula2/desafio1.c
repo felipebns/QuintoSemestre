275e2b6f98bf709f1d014fdd69b01750037d2984
felipebns <felipebns@al.insper.edu.br>
2025-02-17 16:09:47 -0300

    17/02

--- DesProg/Aula2/desafio1.c
+++ DesProg/Aula2/desafio1.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 
-int soma_impares(int v[], int n){
-    if (n-1 < 0){
-        return 0;
+int somaAux(int v[], int inicio, int fim){
+
+    if (inicio == fim){
+        return v[inicio];
     }
-    if (v[n-1] % 2 != 0){
-        return v[n-1] + soma_impares(v, n-1);
-    } else {
-        soma_impares(v, n-1);
-    }
+
+    int meio = (int) (inicio + fim)/2;
+
+    int esquerda = somaAux(v, inicio, meio);
+    int direita = somaAux(v, meio + 1, fim);
+
+    return direita + esquerda;
 }
 
-int soma_pares(int v[], int n){
-    if (n-1 < 0){
-        return soma_impares(v, 7);
-    }
-    if (v[n-1] % 2 == 0){
-        return v[n-1] + soma_pares(v, n-1);
-    } else {
-        soma_pares(v, n-1);
-    }
+int soma (int v[], int n){
+    return somaAux(v, 0, n-1);
 }
+
 
 int main(){
-    int arr[] = {1,2,3,4,5,6,7};
-    int r = soma_pares(arr, 7);
+    int arr[] = {1,2,3,4,5,6};
+    int r = soma(arr, 6);
     printf("r: %d \n", r);
     return 0;
 }
--- DesProg/Aula2/desafio2.c
+++ DesProg/Aula2/desafio2.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 void hanoi(int n, char orig, char aux, char dest){
-    if (n == 0){
+    if (n == 1){
+        printf("mova o disco %d da torre %c para a torre %c \n", n, orig, dest);
         return;
     }
-    printf("mova o disco %d da torre %s para a torre %s", n, )
-    hanoi(n-1, orig, aux, dest);
+    
+    hanoi(n-1, orig, dest, aux);
+    
+    printf("mova o disco %d da torre %c para a torre %c \n", n, orig, dest);
+    
+    hanoi(n-1, aux, orig, dest);
 }
 
 int main(){
